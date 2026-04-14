5f9fa5dd6e3e38d36f7b13ff0b78c0b01df9ee23
Raúl <raul.rodriguez.c.2012@gmail.com>
2026-04-13 21:21:18 -0400

    cambio de modelo gemini

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,4 +4,4 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-export const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
+export const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash-lite' });
